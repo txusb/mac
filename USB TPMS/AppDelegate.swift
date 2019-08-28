a6560203163c52ca6e016896a21c1f5063a7cb48
sam38124 <sam38124@gmail.com>
2019-08-28 15:28:21 +0800

    Initial Commit

---
+++ USB TPMS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  USB TPMS
+//
+//  Created by 王建智 on 2019/8/28.
+//  Copyright © 2019 王建智. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ USB TPMS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  USB TPMS
+//
+//  Created by 王建智 on 2019/8/28.
+//  Copyright © 2019 王建智. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
